6a40a110fd60ebd61b98de45212046fa59a16297
JL-OLEMAR <joseluis19963@gmail.com>
2022-04-13 01:16:07 -0500

    Pre-npm ready

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -6,8 +6,6 @@ import {
   ProductTitle
 } from '../components'
 
-import '../styles/custom-styles.css'
-
 const product = getProducts[0]
 
 export function ShoppingPage() {
@@ -16,11 +14,7 @@ export function ShoppingPage() {
       <h1>Shopping Store</h1>
       <hr />
 
-      <ProductCard
-        className='bg-dark text-white'
-        initialValues={{ count: 4, maxCount: 10 }}
-        product={product}
-      >
+      <ProductCard initialValues={{ count: 4, maxCount: 10 }} product={product}>
         {({
           product,
           count,
@@ -30,25 +24,9 @@ export function ShoppingPage() {
           reset
         }) => (
           <>
-            <ProductImage className='custom-image' />
-            <ProductTitle className='text-bold' />
-            <ProductButtons className='custom-buttons' />
-            {/* Receives for arguments */}
-            <br />
-            <button onClick={reset}>Reset</button>
-            <button onClick={() => increaseBy(-2)}> -2 </button>
-            {!isMaxCountReached ? (
-              <button onClick={() => increaseBy(2)}> +2 </button>
-            ) : (
-              <span style={{ color: '#f00', backgroundColor: 'gold' }}>
-                {' '}
-                Sold out
-              </span>
-            )}
-            {/* ↓ line 21 ↓ */}
-            <p>{`count: ${count ? count : 'No value initial'}`}</p>{' '}
-            <p>{`maxCount: ${maxCount ? maxCount : 'No Value Max'}`}</p>
-            <p>{`isMaxCountReached: ${isMaxCountReached}`}</p>
+            <ProductImage />
+            <ProductTitle />
+            <ProductButtons />
           </>
         )}
       </ProductCard>
